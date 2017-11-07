6cc395b76cee4fe42a9489ad7da84462c25238c2
Laura Caroselli <lcaroselli@outlook.com>
2017-11-06 20:39:12 -0700

    Push up hw

--- public/application.js
+++ public/application.js
@@ -1,7 +1,7 @@
-//Calling the library (socket variable to use) - this will try to make a connection to the server
-var socket = io();
-
-//verifying event on client side and send message back to server
-socket.on('connection', () => {
-  console.log('Connected client side')
-});
+// //Calling the library (socket variable to use) - this will try to make a connection to the server
+// var socket = io();
+//
+// //verifying event on client side and send message back to server
+// socket.on('connection', () => {
+//   console.log('Connected client side')
+// });
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', () => {
-  console.log('Connected to server')
+io.on('connection', (socket) => {
+  socket.broadcast.emit('connected', { text: 'User has connected' })
+
+  socket.on('disconnected', () => {
+    io.emit('disconnected', { text: 'User has disconnected' })
+  });
+
+  socket.on('message', (message) => {
+    io.emit('message', message)
+  });
+
+  socket.on('typing', () => {
+    io.emit('A user is typing')
+  })
+
+  socket.on('notTyping', () => {
+    io.emit('Nobody is typing')
+  })
 });
 
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
